acf0a793b4c22ce0137de7aa1cec1779955578b6
beck <2223969110@qq.com>
2024-06-21 01:05:11 +0800

    chore: 完善打包流程

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import fs from 'node:fs'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [
+        vue(),
+        dts({
+            rollupTypes: true,
+            afterBuild: () => {
+                const old_path = resolve(__dirname, 'dist/vue-qrcode.d.ts')
+                const new_path = resolve(__dirname, 'dist/index.d.ts')
+                fs.renameSync(old_path, new_path)
+            }
+        })
+    ],
     build: {
         target: 'es2015',
         lib: {
@@ -17,11 +31,9 @@ export default defineConfig({
                 globals: {
                     'vue': 'Vue',
                     'qrcode': 'Qrcode'
-                }
+                },
+                exports: 'named'
             }
         }
     },
-    plugins: [
-        vue()
-    ],
 })
